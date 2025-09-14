ce1ac7463f8eb9f900e3d148bccf691992c41c2c
APPLE <apple@APPLEs-MacBook-Air.local>
2025-09-14 17:24:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sandeep",
+    author_email="sandeepdebamishra@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
